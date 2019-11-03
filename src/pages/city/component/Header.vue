95f39238db42efd434eff85246145acb44d4c785
dongdongyeah <986318370@qq.com>
2019-11-04 01:03:18 +0800

    city header

---
+++ src/pages/city/component/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class='header'>
+    城市选择
+    <router-link to='/'>
+      <div class='iconfont back'>&#xe6db;</div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varivles.styl'
+  .header
+    height: 44px
+    line-height: 44px
+    text-align: center
+    font-size: 18px
+    color: #fff
+    background: $bgColor
+    .back
+      position: absolute
+      top: 0
+      left: 5px
+      font-size: 28px
+      color: #fff
+</style>
