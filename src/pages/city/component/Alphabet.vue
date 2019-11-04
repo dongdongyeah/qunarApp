20e8c8ea05a4bfcc9bfa84936df3ab244f04a605
dongdongyeah <986318370@qq.com>
2019-11-04 13:57:08 +0800

    city alphavet

---
+++ src/pages/city/component/Alphabet.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <li>A</li>
+    <li>A</li>
+    <li>A</li>
+    <li>A</li>
+    <li>A</li>
+    <li>A</li>
+    <li>A</li>
+    <li>A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varivles.styl'
+  ul
+    position: absolute
+    z-index: 99
+    top: 85px
+    right: 5px
+    bottom: 0
+    display: flex
+    flex-direction: column
+    justify-content: center
+    li
+      margin-top: 2px
+      color: $bgColor
+</style>
