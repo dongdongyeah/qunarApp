524992c34f3930dc089eb256721208ee6f547a55
dongdongyeah <986318370@qq.com>
2019-11-04 11:58:54 +0800

    city search

---
+++ src/pages/city/component/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class='search-wrapper'>
+    <input class='search' type='text' placeholder='请输入城市名或拼音'/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varivles.styl'
+  .search-wrapper
+    height: 40px
+    line-height: 40px
+    padding: 2px 5px 2px 5px
+    background: $bgColor
+    .search
+      box-sizing: border-box
+      width: 100%
+      height: 25px
+      padding: 0 4px 2px 4px
+      font-size: 13px
+      border-radius: 3px
+</style>
