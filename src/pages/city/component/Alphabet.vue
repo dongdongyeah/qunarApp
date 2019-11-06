87b1f424ef7927a6bc5f08fed27dbf9894e3b48f
dongdongyeah <986318370@qq.com>
2019-11-06 23:13:23 +0800

    srarch input

--- src/pages/city/component/Alphabet.vue
+++ src/pages/city/component/Alphabet.vue
@@ -19,7 +19,9 @@ export default {
   name: 'CityAlphabet',
   data () {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
   },
   props: {
@@ -34,6 +36,9 @@ export default {
       return letters
     }
   },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
   methods: {
     handleClick (e) {
       this.$emit('change', e.target.innerText)
@@ -43,12 +48,16 @@ export default {
     },
     handleTouchMove (e) {
       if (this.touchStatus) {
-        const startY = this.$refs['A'][0].offsetTop
-        const clientY = e.touches[0].clientY - 88
-        const index = Math.floor((clientY - startY) / 14)
-        if (index >= 0 && index < this.letters.length) {
-          this.$emit('change', this.letters[index])
+        if (this.timer) {
+          clearTimeout(this.timer)
         }
+        this.timer = setTimeout(() => {
+          const clientY = e.touches[0].clientY - 88
+          const index = Math.floor((clientY - this.startY) / 14)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
       }
     },
     handleTouchEnd (touchStatus) {
@@ -62,7 +71,7 @@ export default {
 @import '~styles/varivles.styl'
   ul
     position: absolute
-    z-index: 99
+    z-index: 2
     top: 85px
     right: 0
     bottom: 0
--- src/pages/city/component/Search.vue
+++ src/pages/city/component/Search.vue
@@ -1,12 +1,53 @@
 <template>
   <div class='search-wrapper'>
-    <input class='search' type='text' placeholder='请输入城市名或拼音'/>
+    <input v-model='keyword' class='search' type='text' placeholder='请输入城市名或拼音'/>
+    <ul class='search-list' v-show='hasInput'>
+      <li
+       v-for='item of keywordList'
+        :key='item.id'
+        class='border-bottom'
+      >
+        {{item.name}}
+      </li>
+      <li v-show='hasNotData'>没有找到匹配数据</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  data () {
+    return {
+      keyword: '',
+      keywordList: []
+    }
+  },
+  props: {
+    cities: Object
+  },
+  computed: {
+    hasNotData () {
+      return !this.keywordList.length
+    },
+    hasInput () {
+      return this.keyword
+    }
+  },
+  watch: {
+    keyword () {
+      const list = []
+      for (let key in this.cities) {
+        this.cities[key].forEach((value) => {
+          if (value.name.indexOf(this.keyword) > -1 ||
+          value.spell.indexOf(this.keyword) > -1) {
+            list.push(value)
+          }
+        })
+      }
+      this.keywordList = list
+    }
+  }
 }
 </script>
 
@@ -25,4 +66,16 @@ export default {
       padding: 0 4px 2px 4px
       font-size: 12px
       border-radius: 3px
+    .search-list
+      z-index: 3
+      position: absolute
+      top: 88px
+      left: 0
+      right: 0
+      bottom: 0
+      background: #eee
+      li
+        line-height: 30px
+        text-indent: 5px
+        background: #fff
 </style>
